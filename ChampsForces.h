eabb01048bf4fda9194a24156d384e1b087f1438
chloechochon <102471688+chloechochon@users.noreply.github.com>
2022-05-19 14:04:40 +0200

    Update ChampsForces.h

--- ChampsForces.h
+++ ChampsForces.h
@@ -2,6 +2,7 @@
 #include "ObjetMobile.h"
 #include "ex_vecteur.h"
 #include "Systeme.h"
+#include "constantes.h"
 #include <iostream>
 
 class ChampForces {
@@ -9,7 +10,7 @@ class ChampForces {
     //les methodes
     void agit_sur(ObjetMobile& obj);
     Vecteur getintensite() const;
-	virtual ChampForces* copie() const;
+    virtual ChampForces* copie() const;
 
 	//constructeur
 	ChampForces (Vecteur F)
@@ -36,17 +37,17 @@ class Vent : public ChampForces {
 	Vent (Vecteur inten,Vecteur orin, Vecteur nor, Vecteur l, Vecteur hau, double pro) : ChampForces(inten), origine(orin), normale(nor), largeur(l), hauteur(hau), profondeur(pro) { 
 		//au cas ou les vecteurs ne soit pas perpendiculaires
 		
-		if (not ((largeur|normale)==0)) {
+		if ((largeur|normale) > epsilon) {
 		Vecteur a(!normale); //a prend la direction de normal
 			largeur= largeur - (largeur*a)*a;
 		}	
 		
-		if (not ((hauteur|normale)==0)) {
+		if ((hauteur|normale) > epsilon) {
 		Vecteur a(!normale); //a prend la direction de normal
 			hauteur= hauteur - (hauteur*a)*a;
 		}
 		
-		if (not ((largeur|hauteur)==0)) { 
+		if ((largeur|hauteur) > epsilon) { 
 			 Vecteur a(!largeur); //a prend la direction de largeur
 			hauteur = hauteur - (hauteur*a)*a;
 		}	
